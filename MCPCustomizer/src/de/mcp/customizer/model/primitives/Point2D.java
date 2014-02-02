d968fb18a061cd68b306a19b5c05b80b708dfb32
Dennis Lewandowski <dennis.lewandowski@rwth-aachen.de>
2014-02-02 10:58:40 +0100

    Renamed Vector to Point

--- MCPCustomizer/src/de/mcp/customizer/model/primitives/Point2D.java
+++ MCPCustomizer/src/de/mcp/customizer/model/primitives/Point2D.java
@@ -4,13 +4,13 @@ import java.io.Serializable;
 
 import toxi.geom.Vec2D;
 
-public class Vector2D implements Serializable{
+public class Point2D implements Serializable{
 
 	private static final long serialVersionUID = 3944945759160627223L;
 
 	private transient Vec2D vector;
 	
-	public Vector2D(float x, float y) {
+	public Point2D(float x, float y) {
 		this.vector = new Vec2D(x,y);
 	}
 	
