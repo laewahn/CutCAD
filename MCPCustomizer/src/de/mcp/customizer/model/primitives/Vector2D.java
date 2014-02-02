711d6a56d3c2cdccbf1dd72fc4b324e918ef62bc
Dennis Lewandowski <dennis.lewandowski@rwth-aachen.de>
2014-02-02 10:58:04 +0100

    Added serialisable vector class

---
+++ MCPCustomizer/src/de/mcp/customizer/model/primitives/Vector2D.java
@@ -0,0 +1,24 @@
+package de.mcp.customizer.model.primitives;
+
+import java.io.Serializable;
+
+import toxi.geom.Vec2D;
+
+public class Vector2D implements Serializable{
+
+	private static final long serialVersionUID = 3944945759160627223L;
+
+	private transient Vec2D vector;
+	
+	public Vector2D(float x, float y) {
+		this.vector = new Vec2D(x,y);
+	}
+	
+	public float x() {
+		return this.vector.x();
+	}
+	
+	public float y() {
+		return this.vector.y();
+	}
+}
